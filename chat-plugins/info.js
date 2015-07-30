50b09c7365be8e446ff0a5458dbfcd751c685e7c
Austin <pingas311@hotmail.ca>
2015-07-29 21:58:36 -0600

    Add Operator

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -1706,6 +1706,7 @@ var commands = exports.commands = {
 		if (!this.canBroadcast()) return;
 		this.sendReplyBox(
 			"+ <b>Voice</b> - They can use ! commands like !groups, and talk during moderated chat<br />" +
+			"$ <b>Operator</b> - The above, they can mute, warn, and redirect to any room<br />" +
 			"% <b>Driver</b> - The above, and they can mute. Global % can also lock users and check for alts<br />" +
 			"@ <b>Moderator</b> - The above, and they can ban users<br />" +
 			"&amp; <b>Leader</b> - The above, and they can promote to moderator and force ties<br />" +
